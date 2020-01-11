c7b597e8564cba358468e8e407ec182bca057da0
Alexander Ghosh <44024068+AGWDW@users.noreply.github.com>
2020-01-11 21:17:47 +0000

    Physics programmed not tested or implemented in any way

--- openGl Mesh/Game/World/Physics.cpp
+++ openGl Mesh/Game/World/Physics.cpp
@@ -1,4 +1,221 @@
 #include "Physics.h"
 namespace Physics {
+	Object::Object() {
+		mass = 100;
+	}
+	Object::Object(GLfloat mass, Material& material) {
+		setMass(mass);
+		setMaterial(material);
+	}
+	void Object::addForce(GLfloat force, glm::vec3 direction) {
+		glm::vec3 force = direction * force;
+		acceleration += force / mass;
+	}
+	void Object::addForce(glm::vec3 force) {
+		acceleration += force / mass;
+	}
+	void Object::addForce(GLfloat force) {
+		acceleration += force / mass;
+	}
+	void Object::addAngularForce(GLfloat force, glm::vec3 rotaion) {
+		glm::vec3 force = rotaion * force;
+		angularAcceleration += force / mass;
+	}
+	void Object::addAngularForce(glm::vec3 force) {
+		angularAcceleration += force / mass;
+	}
+	void Object::addAngularForce(GLfloat force) {
+		angularAcceleration += force / mass;
+	}
+
+	void Object::clampVelocity(const glm::vec3& max, const glm::vec3& min) {
+		velocityClamps = { max, min };
+		doVelocityClamp = GL_TRUE;
+	}
+	void Object::clampSpeed(const GLfloat& max, const GLfloat& min) {
+		speedClamps = { max, min };
+		doSpeedClamp = GL_TRUE;
+	}
+	void Object::clampAngularVelocity(const glm::vec3& max, const glm::vec3& min) {
+		angularVelocityClamps = { max, min };
+		doAngularVelocityClamp = GL_TRUE;
+	}
+	void Object::clampAngularSpeed(const GLfloat& max, const GLfloat& min) {
+		angularSpeedClamps = { max, min };
+		doAngularSpeedClamp = GL_TRUE;
+	}
+
+	// getters
+	GLboolean& Object::getKinematic() {
+		return isKinematic;
+	}
+	GLboolean& Object::getPhysical() {
+		return isPhysical;
+	}
+	glm::vec3& Object::getPosition() {
+		return position;
+	}
+	glm::vec3& Object::getRotation() {
+		return rotation;
+	}
+	glm::vec3& Object::getVelocity() {
+		return velocity;
+	}
+	glm::vec3& Object::getAcceleration() {
+		return acceleration;
+	}
+	GLfloat Object::getSpeed() {
+		return glm::length(velocity);
+	}
+	GLfloat& Object::getMass() {
+		return mass;
+	}
+	glm::vec3& Object::getAngluarVelocity() {
+		return angularVelocity;
+	}
+	glm::vec3& Object::getAngularAcceleration() {
+		return angularAcceleration;
+	}
+	GLfloat Object::getAngularSpeed() {
+		return glm::length(angularVelocity);
+	}
+	Material& Object::getMaterial() {
+		return material;
+	}
+
+	//setters
+	void Object::setKinematic(const GLboolean& value) {
+		isKinematic = value;
+	}
+	void Object::setPhysical(const GLboolean& value) {
+		isPhysical = value;
+	}
+	void Object::setPosition(const glm::vec3& value) {
+		position = value;
+	}
+	void Object::setRotation(const glm::vec3& value) {
+		rotation = value;
+	}
+	void Object::setVelocity(const glm::vec3& value) {
+		velocity = value;
+	}
+	void Object::setAcceleration(const glm::vec3& value) {
+		acceleration = value;
+	}
+	void Object::setMass(const GLfloat& value) {
+		mass = value;
+	}
+	void Object::setAngularVelocity(const glm::vec3& value) {
+		angularVelocity = value;
+	}
+	void Object::setAngularAcceleration(const glm::vec3& value) {
+		angularAcceleration = value;
+	}
+	void Object::setMaterial(const Material& value) {
+		material = value;
+	}
+
+	void Object::clamp() {
+		if (doVelocityClamp) {
+			glm::vec3 vel = getVelocity();
+			glm::vec3 min = velocityClamps.Min;
+			if (vel.x < min.x) {
+				vel.x = min.x;
+			}
+			if (vel.y < min.y) {
+				vel.y = min.y;
+			}
+			if (vel.z < min.z) {
+				vel.z = min.z;
+			}
+			glm::vec3 max = velocityClamps.Max;
+			if (vel.x < max.x) {
+				vel.x = max.x;
+			}
+			if (vel.y < max.y) {
+				vel.y = max.y;
+			}
+			if (vel.z < max.z) {
+				vel.z = max.z;
+			}
+		}
+		if (doSpeedClamp) {
+			GLfloat speed = getSpeed();
+			if (speed < speedClamps.Min) {
+				glm::vec3 unit = glm::normalize(getVelocity());
+				unit *= speedClamps.Min;
+				setPosition(unit);
+			}
+			if (speed > speedClamps.Max) {
+				glm::vec3 unit = glm::normalize(getVelocity());
+				unit *= speedClamps.Max;
+				setPosition(unit);
+			}
+		}
+
+		if (doAngularVelocityClamp) {
+			glm::vec3 vel = getAngluarVelocity();
+			glm::vec3 min = angularVelocityClamps.Min;
+			if (vel.x < min.x) {
+				vel.x = min.x;
+			}
+			if (vel.y < min.y) {
+				vel.y = min.y;
+			}
+			if (vel.z < min.z) {
+				vel.z = min.z;
+			}
+			glm::vec3 max = angularVelocityClamps.Max;
+			if (vel.x < max.x) {
+				vel.x = max.x;
+			}
+			if (vel.y < max.y) {
+				vel.y = max.y;
+			}
+			if (vel.z < max.z) {
+				vel.z = max.z;
+			}
+		}
+		if (doAngularSpeedClamp) {
+			GLfloat speed = getAngularSpeed();
+			if (speed < angularSpeedClamps.Min) {
+				glm::vec3 unit = glm::normalize(getVelocity());
+				unit *= speedClamps.Min;
+				setPosition(unit);
+			}
+			if (speed > angularSpeedClamps.Max) {
+				glm::vec3 unit = glm::normalize(getVelocity());
+				unit *= speedClamps.Max;
+				setPosition(unit);
+			}
+		}
+	}
+
+
+
+	GLboolean BoxCollider::checkCollision(Object& object) {
+		glm::vec3 pos = object.getPosition();
+		glm::vec3 upper = posiotion + size;
+		glm::vec3 lower = posiotion - size;
+		if (pos.x < upper.x && pos.y < upper.y && pos.z < upper.z) {
+			if (pos.x > lower.x&& pos.y > lower.y&& pos.z > lower.z) {
+				return GL_FALSE;
+			}
+		}
+		return GL_FALSE;
+	}
+
+	template<typename T>
+	void Engine<T>::doUpdates() {
+		for (auto& update : updateBuffer) {
+			if (update.Tag == TAG::COLLISION) {
+				// get the closest chunk
+				// check its collison mesh
+				// if collied dont do update
+				// else do update
+			}
+		}
+	}
+
 
 };
--- openGl Mesh/Game/World/Physics.h
+++ openGl Mesh/Game/World/Physics.h
@@ -2,36 +2,131 @@
 #include <GL/glew.h>
 #include <glm.hpp>
 #include <vector>
+#include <array>
+
+#include "World.h"
 namespace Physics {
+	class Material; 
+	template<typename T>
+	class Engine;
+	class Colider;
+	enum TAG {
+		COLLISION
+	};
+	template<typename T>
+	struct Update {
+		TAG Tag;
+		T Data;
+		glm::vec3 Positon;
+	};
+	template<typename T>
+	struct Clamp {
+		T Min;
+		T Max;
+		Clamp(T min, T max) {
+			Min = min;
+			Max = max;
+		}
+	};
 	class Object
 	{
 	public:
 		Object();
-		void addForce(glm::vec3 direction);
+		Object(GLfloat mass, Material& material);
+		void addForce(GLfloat force, glm::vec3 direction);
+		void addForce(glm::vec3 force);
+		void addForce(GLfloat force);
+		void addAngularForce(GLfloat force, glm::vec3 direction);
+		void addAngularForce(glm::vec3 force);
+		void addAngularForce(GLfloat force);
+		void clampVelocity(const glm::vec3& max, const glm::vec3& min);
+		void clampSpeed(const GLfloat& max, const GLfloat& min);
+		void clampAngularVelocity(const glm::vec3& max, const glm::vec3& min);
+		void clampAngularSpeed(const GLfloat& max, const GLfloat& min);
 
+		void update();
+
+		// getters
+		GLboolean& getKinematic();
+		GLboolean& getPhysical();
+		glm::vec3& getPosition();
+		glm::vec3& getRotation();
+		glm::vec3& getVelocity();
+		glm::vec3& getAcceleration();
+		GLfloat getSpeed();
+		GLfloat& getMass();
+		glm::vec3& getAngluarVelocity();
+		glm::vec3& getAngularAcceleration();
+		GLfloat getAngularSpeed();
+		Material& getMaterial();
+		BoxCollider& getCollider();
+		// setter
+		void setKinematic(const GLboolean& value);
+		void setPhysical(const GLboolean& value);
+		void setPosition(const glm::vec3& value);
+		void setRotation(const glm::vec3& value);
+		void setVelocity(const glm::vec3& value);
+		void setAcceleration(const glm::vec3& value);
+		void setMass(const GLfloat& value);
+		void setAngularVelocity(const glm::vec3& value);
+		void setAngularAcceleration(const glm::vec3& value);
+		void setMaterial(const Material& value);
+		void setCollider(const BoxCollider& value);
 	private:
+		GLfloat mass;
 		GLboolean isKinematic;
 		GLboolean isPhysical;
 		glm::vec3 position;
-		glm::vec3 rotation;
 		glm::vec3 velocity;
 		glm::vec3 acceleration;
+		glm::vec3 rotation;
+		glm::vec3 angularVelocity;
+		glm::vec3 angularAcceleration;
+		Material material;
+		BoxCollider collider;
+
+		GLboolean doVelocityClamp;
+		GLboolean doSpeedClamp;
+		GLboolean doAngularVelocityClamp;
+		GLboolean doAngularSpeedClamp;
+		Clamp<glm::vec3> velocityClamps;
+		Clamp<GLfloat> speedClamps;
+		Clamp<glm::vec3> angularVelocityClamps;
+		Clamp<GLfloat> angularSpeedClamps;
+
+		void clamp();
 	};
 	class Material {
 	public:
 		Material();
-
+		GLboolean isNull();
 	private:
+		GLboolean null;
 		GLfloat frictionConstant;
 		GLfloat bouncines;
 		glm::vec3 position;
 	};
+	class BoxCollider {
+	public:
+		BoxCollider();
+		
+	private:
+		glm::vec3& posiotion;
+		GLfloat size;
+		GLboolean checkCollision(Object& object);
+	};
+	template<typename T>
 	class Engine {
 	public:
-
+		static World world;
+		static void addObject(const Object& object);
+		static void doUpdates();
 	private:
-		std::vector<Object> objects;
-		std::vector<Material> materials;
+		static std::vector<Update<T>> updateBuffer; // needs a type
+		static std::vector<Object> objects;
+		static std::vector<Material> materials;
+		static std::vector<BoxCollider> colliders;
+		static void doCollision();
 	};
 };
 
